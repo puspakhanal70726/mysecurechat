db7bc6d3a50cd141b4ff742aa7e7a538c6c1a7f4
Puspa Khanal <31549620+puspakhanal70726@users.noreply.github.com>
2018-12-05 16:07:39 -0500

    no message

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'conversations#index'
+  resources :personal_messages, only: [:create]
+resources :conversations, only: [:index, :show]
+
+resources :users, only: [:index]
+resources :personal_messages, only: [:new, :create]
+end
